97457f67b7b5d581ac935c1e6f8ca296cc0b286d
Artur Gajewski <info@arturgajewski.com>
2013-06-19 09:12:47 +0300

    Made data fixtures more realistic.

--- src/Cobase/AppBundle/DataFixtures/ORM/PostFixtures.php
+++ src/Cobase/AppBundle/DataFixtures/ORM/PostFixtures.php
@@ -13,19 +13,19 @@ class PostsFixtures extends AbstractFixture implements OrderedFixtureInterface
     {
         $post1 = new Post();
         $post1->setUser($manager->merge($this->getReference('user-1')));
-        $post1->setContent('According to Google...');
+        $post1->setContent('NSA chief: We thwarted more than 50 terror attacks: http://rt.com/usa/attacks-50-nsa-alexander-891');
         $post1->setGroup($manager->merge($this->getReference('group-1')));
         $manager->persist($post1);
 
         $post2 = new Post();
         $post2->setUser($manager->merge($this->getReference('user-2')));
-        $post2->setContent('New PHP version released');
+        $post2->setContent('Wimbledon 2013: The state of grassroots & elite tennis in Britain: http://www.bbc.co.uk/sport/0/tennis/22912754');
         $post2->setGroup($manager->merge($this->getReference('group-2')));
         $manager->persist($post2);
 
         $post3 = new Post();
         $post3->setUser($manager->merge($this->getReference('user-3')));
-        $post3->setContent('Here are the latest news!');
+        $post3->setContent('Justin Rose: A wonderful golfer who tamed the Merion beast. Read about at http://www.bbc.co.uk/sport/0/golf/22941311');
         $post3->setGroup($manager->merge($this->getReference('group-2')));
         $manager->persist($post3);
 
