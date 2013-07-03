3b8e3223e5bb97d762c914c9f09260b927ce241e
Artur Gajewski <info@arturgajewski.com>
2013-07-03 17:00:00 +0300

    Added ago twig filter to post dates

--- src/Cobase/AppBundle/Twig/Extensions/CobaseAppExtension.php
+++ src/Cobase/AppBundle/Twig/Extensions/CobaseAppExtension.php
@@ -7,7 +7,6 @@ class CobaseAppExtension extends \Twig_Extension
     public function getFilters()
     {
         return array(
-            'created_ago' => new \Twig_Filter_Method($this, 'createdAgo'),
             'show_maxlen' => new \Twig_Filter_Method($this, 'showMaxLen'),
         );
     }
@@ -22,41 +21,6 @@ class CobaseAppExtension extends \Twig_Extension
         return $string;
     }
     
-    public function createdAgo(\DateTime $dateTime)
-    {
-        $delta = time() - $dateTime->getTimestamp();
-        if ($delta < 0)
-            throw new \Exception("createdAgo is unable to handle dates in the future");
-
-        $duration = "";
-        if ($delta < 60)
-        {
-            // Seconds
-            $time = $delta;
-            $duration = $time . " second" . (($time === 0 || $time > 1) ? "s" : "") . " ago";
-        }
-        else if ($delta < 3600)
-        {
-            // Mins
-            $time = floor($delta / 60);
-            $duration = $time . " minute" . (($time > 1) ? "s" : "") . " ago";
-        }
-        else if ($delta < 86400)
-        {
-            // Hours
-            $time = floor($delta / 3600);
-            $duration = $time . " hour" . (($time > 1) ? "s" : "") . " ago";
-        }
-        else
-        {
-            // Days
-            $time = floor($delta / 86400);
-            $duration = $time . " day" . (($time > 1) ? "s" : "") . " ago";
-        }
-
-        return $duration;
-    }
-
     public function getName()
     {
         return 'Cobase_app_extension';
