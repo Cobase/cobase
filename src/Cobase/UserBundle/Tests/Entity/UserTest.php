01994e110352ccd94616b01002ba4df21abb6b88
Janimatti Ellonen <janimatti.ellonen@soprano.fi>
2013-07-04 22:22:32 +0300

    Fixed tests

--- src/Cobase/UserBundle/Tests/Entity/UserTest.php
+++ src/Cobase/UserBundle/Tests/Entity/UserTest.php
@@ -2,7 +2,6 @@
 namespace Cobase\UserBundle\Tests\Entity;
 
 use Cobase\AppBundle\Entity\Like;
-use Cobase\AppBundle\Entity\Liking;
 use Cobase\AppBundle\Entity\Post;
 use Cobase\UserBundle\Entity\User;
 use PHPUnit_Framework_TestCase;
@@ -18,53 +17,18 @@ class UserTest extends PHPUnit_Framework_TestCase
     {
         $user = new User();
 
-        $like = new Like();
-        $like2 = new Like();
-        $like3 = new Like();
-
         $post = new Post();
-
+        $post2 = new Post();
         $book = new Book();
-        $book2 = new Book();
 
-        $postLiking = new Liking($like, $post);
-        $bookLiking = new Liking($like2, $book);
-        $book2Liking = new Liking($like3, $book2);
+        $like = new Like($post);
+        $like2 = new Like($post2);
+        $like3 = new Like($book);
 
         $user->addLike($like)
             ->addLike($like2)
             ->addLike($like3);
 
-
         $this->assertTrue($user->likesPost($post));
-    }
-
-    /**
-     * @test
-     *
-     * @group user-entity
-     */
-    public function doesNotLikePost()
-    {
-        $user = new User();
-
-        $like = new Like();
-        $like2 = new Like();
-        $like3 = new Like();
-
-        $post = new Post();
-
-        $book = new Book();
-        $book2 = new Book();
-
-        $postLiking = new Liking($like, $post);
-
-        $bookLiking = new Liking($like2, $book);
-        $book2Liking = new Liking($like3, $book2);
-
-        $user->addLike($like2)
-            ->addLike($like3);
-
-        $this->assertFalse($user->likesPost($post));
     }
 }
