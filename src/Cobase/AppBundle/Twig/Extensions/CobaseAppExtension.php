33a72402c38fb46142d11623fca9985143c0f544
Artur Gajewski <info@arturgajewski.com>
2013-07-09 19:29:32 +0300

    Added new flag to new groups

--- src/Cobase/AppBundle/Twig/Extensions/CobaseAppExtension.php
+++ src/Cobase/AppBundle/Twig/Extensions/CobaseAppExtension.php
@@ -8,9 +8,18 @@ class CobaseAppExtension extends \Twig_Extension
     {
         return array(
             'show_maxlen' => new \Twig_Filter_Method($this, 'showMaxLen'),
+            'show_newflag' => new \Twig_Filter_Method($this, 'showNewFlag'),
         );
     }
 
+    /**
+     * Crop string at the given length and append ending string.
+     * 
+     * @param $value
+     * @param $maxlen
+     * @param string $ending
+     * @return string
+     */
     public function showMaxLen($value, $maxlen, $ending = '')
     {
         if (strlen($value) > $maxlen) {
@@ -20,7 +29,30 @@ class CobaseAppExtension extends \Twig_Extension
         }
         return $string;
     }
-    
+
+    /**
+     * Render new flag if given date is less than five days
+     * 
+     * @param \DateTime $dateTime
+     * @return string
+     * @throws \Exception
+     */
+    public function showNewFlag(\DateTime $dateTime)
+    {
+        $delta = time() - $dateTime->getTimestamp();
+        if ($delta < 0)
+        throw new \Exception("Ago is unable to handle dates in the future");
+        
+        $newFlagString = "";
+        $time = floor($delta / 86400);
+        
+        if ($time < 5) {
+            $newFlagString = '<span class="label label-info">New</span>';
+        }
+        
+        return $newFlagString;
+    }
+        
     public function getName()
     {
         return 'Cobase_app_extension';
