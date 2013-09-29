ea363ddbe8296fe9beee9b0b7bcc761eceedd21c
Sami Tikka <stikka@iki.fi>
2013-09-29 14:19:32 +0300

    integrate with fosuserbundle

--- src/Cobase/AppBundle/Entity/Comment.php
+++ src/Cobase/AppBundle/Entity/Comment.php
@@ -4,13 +4,15 @@ namespace Cobase\AppBundle\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
 use FOS\CommentBundle\Entity\Comment as BaseComment;
+use FOS\CommentBundle\Model\SignedCommentInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 /**
  * @ORM\Entity
  * @ORM\Table(name="comments")
  * @ORM\ChangeTrackingPolicy("DEFERRED_EXPLICIT")
  */
-class Comment extends BaseComment
+class Comment extends BaseComment implements SignedCommentInterface
 {
     /**
      * @ORM\Id
@@ -26,4 +28,40 @@ class Comment extends BaseComment
      * @ORM\ManyToOne(targetEntity="Cobase\AppBundle\Entity\Thread")
      */
     protected $thread;
+
+    /**
+     * Author of the comment
+     *
+     * @ORM\ManyToOne(targetEntity="Cobase\UserBundle\Entity\User")
+     * @var User
+     */
+    protected $author;
+
+    /**
+     * @param UserInterface $author
+     */
+    public function setAuthor(UserInterface $author)
+    {
+        $this->author = $author;
+    }
+
+    /**
+     * @return User|UserInterface
+     */
+    public function getAuthor()
+    {
+        return $this->author;
+    }
+
+    /**
+     * @return string
+     */
+    public function getAuthorName()
+    {
+        if (null === $this->getAuthor()) {
+            return 'Anonymous';
+        }
+
+        return $this->getAuthor()->getUsername();
+    }
 }
