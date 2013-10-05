9d8a3dfdd0f85c34e595e583105f7577e5d8b430
Janimatti Ellonen <janimatti.ellonen@gmail.com>
2013-10-05 13:44:13 +0100

    Now tests use sqlite

---
+++ src/Cobase/AppBundle/Tests/Functional/DatabaseTest.php
@@ -0,0 +1,26 @@
+<?php
+namespace Cobase\AppBundle\Tests\Functional;
+
+use Cobase\AppBundle\Entity\Post;
+use CoBase\Component\Test\ServiceTestCase;
+use Cobase\UserBundle\Entity\User;
+
+/**
+ * @group functional
+ */
+class DatabaseTest extends ServiceTestCase
+{
+    /**
+     * @test
+     *
+     * @group cur
+     */
+    public function assertSqliteDbIsUsed()
+    {
+        $post = new Post();
+        $post->setContent('TestContent');
+
+        $this->getEntityManager()->persist($post);
+        $this->getEntityManager()->flush();
+    }
+}
