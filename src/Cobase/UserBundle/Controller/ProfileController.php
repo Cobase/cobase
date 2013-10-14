6d2838ec147fc8fb0e4077fc1a33c1b27d7bdd82
Artur Gajewski <info@arturgajewski.com>
2013-10-14 13:09:43 +0300

    Direct to profile/edit

--- src/Cobase/UserBundle/Controller/ProfileController.php
+++ src/Cobase/UserBundle/Controller/ProfileController.php
@@ -34,20 +34,8 @@ class ProfileController extends BaseController
      */
     public function showAction()
     {
-        $user = $this->container->get('security.context')->getToken()->getUser();
-        if (!is_object($user) || !$user instanceof UserInterface) {
-            throw new AccessDeniedException('This user does not have access to this section.');
-        }
-
-        return $this->container->get('templating')->renderResponse(
-            'FOSUserBundle:Profile:show.html.'.$this->container->getParameter('fos_user.template.engine'),
-            array(
-                'user'          => $user,
-                'subscriptions' => $this->getSubscriptions(),
-                'latestGroups'  => $this->getGroupService()->getLatestPublicGroups(10),
-                'latestUsers'   => $this->getUserService()->getLatestUsers(10),
-            )
-        );
+        $request = $this->getRequest();
+        return $this->editAction($request);
     }
 
     /**
